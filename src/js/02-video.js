c93c2e9bf2fe78ba9c4ffd74ae5edebf713ff525
Kamila0301 <bogda0301@gmail.com>
2023-09-15 17:12:32 +0300

    update

---
+++ src/js/02-video.js
@@ -0,0 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
